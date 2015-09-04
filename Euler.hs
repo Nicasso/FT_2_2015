903e6169a021a2208b8815d900e045736aa220b1
astavares <astavares4@gmail.com>
2015-09-04 11:59:32 +0200

    project euler problem 9 simplified

--- Euler.hs
+++ Euler.hs
@@ -1,7 +1,4 @@
 -- Problem 9: Special Pythagorean triplet
+pythagorean :: [(Integer, Integer, Integer)]
+pythagorean = [ (x, y, z) | z <- [1 .. 1000], y <- [1 .. z], x <- [1 .. y], (x^2 + y^2 == z^2) && (x + y + z == 1000)]
 
-pythagorean = head (filter (/=(0,0,0)) [ pythagoreanChecker x y z | z <- [1 .. 1000], y <- [1 .. z], x <- [1 .. y]])
-
-pythagoreanChecker :: Integer -> Integer -> Integer -> (Integer,Integer,Integer)
-pythagoreanChecker x y z | ((x^2) + (y^2) == (z^2)) && (x+y+z) == 1000 = (x,y,z)
-                         | otherwise = (0,0,0)

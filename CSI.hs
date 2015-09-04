de5b1be7db0d431709278362f808cdd9f82e7f4f
rdmk <robinkulhan@wcw-stud-145-109-13-9.wireless.uva.nl>
2015-09-04 09:53:33 +0200

    Adding comments and removing unused function

--- CSI.hs
+++ CSI.hs
@@ -1,8 +1,12 @@
+-- Week 1: CSI.hs
+-- FT_02: Nico de Groot, Robin Kulhan, Andre Tavares
+
 data Boy = Matthew | Peter | Jack | Arnold | Carl 
            deriving (Eq,Show)
 
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
+-- Declarations of all the things the boys said
 matthewSays, peterSays, jackSays, arnoldSays, carlSays :: Boy -> Bool
 matthewSays = \x -> not (x == Matthew) && not (x == Carl)
 peterSays = \x -> x == Matthew || x == Jack
@@ -10,13 +14,7 @@ jackSays = \x -> not (matthewSays x) && not (peterSays x)
 arnoldSays = \x -> matthewSays x /= peterSays x
 carlSays = \x -> not (arnoldSays x)
 
-matthewAccusers, peterAccusers, jackAccusers, arnoldAccusers, carlAccusers :: Boy -> [Boy]
-matthewAccusers x = [Peter, Jack]
-peterAccusers x = [Jack]
-jackAccusers x = [Peter]
-arnoldAccusers x = [Carl]
-carlAccusers x = []
-
+-- Return a list of accusers for each boy
 accusers :: Boy -> [Boy]
 accusers x | x == Matthew = [Peter, Jack, Arnold]
            | x == Peter = [Jack, Arnold]
@@ -24,27 +22,18 @@ accusers x | x == Matthew = [Peter, Jack, Arnold]
            | x == Arnold = [Carl]
            | x == Carl = []
 
+-- Checks which boys(s) are guilty
 guilty :: [Boy]
 guilty = [ (x) | x <- [Matthew, Peter, Jack, Arnold, Carl], (((matthewSays (x) == peterSays (x)) == jackSays (x)) == arnoldSays (x)) == carlSays (x) ]
 
+-- Checks for each of the boys if they are honest
 honest :: [Boy]
 honest = [ (x) | x <- [Matthew, Peter, Jack, Arnold, Carl], checkHonesty x]
 
+-- Checks if their declariations are true or false
 checkHonesty :: Boy -> Bool
 checkHonesty x | x == Matthew = not (elem Carl guilty) && not (elem Matthew guilty)
                | x == Peter = elem Matthew guilty || elem Jack guilty
-               | x == Jack = not checkMatthew && not checkPeter
-               | x == Arnold = checkMatthew /= checkPeter
-               | x == Carl = not checkArnold
-
--- Honest: Peter, Matthew, Carl
--- False: Jack, Arnold
-
-data Creature = Lady | Tiger deriving (Eq,Show)
-
-sign1, sign2 :: (Creature,Creature) -> Bool
-sign1 (x,y) = x == Lady && y == Tiger
-sign2 (x,y) = x /= y
-
-solution1 :: [(Creature,Creature)]
-solution1 = [ (x,y) | x <- [Lady,Tiger], y <- [Lady,Tiger], sign1 (x,y) /= sign2 (x,y) ]
+               | x == Jack = not (checkHonesty Matthew) && not (checkHonesty Peter)
+               | x == Arnold = checkHonesty Matthew /= checkHonesty Peter
+               | x == Carl = not (checkHonesty Arnold)

c38fdfe3e0704274d02c2943ca941b2eef8c52db
Nico de Groot <nico@nicasso.nl>
2015-09-07 14:59:49 +0200

    Fixed triangle

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -7,19 +7,19 @@ data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle  a b c | not((a + b > c) && (b + c > a) && (c + a > b)) = NoTriangle
+triangle  a b c | (a + b <= c) || (b + c <= a) || (c + a <= b) = NoTriangle
                 | (a == b) && (b == c) = Equilateral
                 | (a == b) || (b == c) || (a == c) = Isosceles
                 | (a^2 + b^2 == c^2) || (a^2 + c^2 == b^2) || (b^2 + c^2 == a^2) = Rectangular
                 | otherwise = Other
-                
-isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation a b = elem a (permutations b)
 
 -- ANDRE T. VERSION
 triangleA :: Integer -> Integer -> Integer -> Shape
 triangleA x y z  | ((x + y <= z) || (x + z <= y) || (z + y <= x)) = NoTriangle
-				 | (x == y) && (x == z) = Equilateral 
-				 | (((x == y) && (x /= z)) || ((x == z) && (x /= y)) || ((y == z) && (y /= x))) = Isosceles	
-				 | (x^2 + y^2 == z^2) || (z^2 + y^2 == x^2) || (x^2 + z^2 == y^2) = Rectangular		
-				 | otherwise = Other
+         | (x == y) && (x == z) = Equilateral 
+         | (((x == y) && (x /= z)) || ((x == z) && (x /= y)) || ((y == z) && (y /= x))) = Isosceles 
+         | (x^2 + y^2 == z^2) || (z^2 + y^2 == x^2) || (x^2 + z^2 == y^2) = Rectangular   
+         | otherwise = Other
+
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation a b = elem a (permutations b)

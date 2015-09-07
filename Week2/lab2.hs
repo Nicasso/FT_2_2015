667b8cdbc5f3ad4434f483c37b217c859ba9d68c
Nico de Groot <nico@nicasso.nl>
2015-09-07 13:16:48 +0200

    Fixid stopid fault

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -1,4 +1,7 @@
-> module Lab2 where
-> 
-> import Data.List
-> import System.Random
+module Lab2 where
+ 
+import Data.List
+import System.Random
+
+data Shape = NoTriangle | Equilateral 
+            | Isosceles | Rectangular | Other deriving (Eq,Show)

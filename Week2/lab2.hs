883e812c72c43aacd43fdd8eaef75099547ae9bd
astavares <astavares4@gmail.com>
2015-09-07 14:16:51 +0200

    triangle my version

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -7,8 +7,17 @@ data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
+
 triangle  a b c | not((a + b > c) && (b + c > a) && (c + a > b)) = NoTriangle
                 | (a == b) && (b == c) = Equilateral
                 | (a == b) || (b == c) || (a == c) = Isosceles
                 | (a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)= Rectangular
                 | otherwise = Other
+
+-- ANDRE T. VERSION
+--triangle x y z  | ((x + y <= z) || (x + z <= y) || (z + y <= x)) = NoTriangle
+--				| (x == y) && (x == z) = Equilateral 
+--				| (((x == y) && (x /= z)) || ((x == z) && (x /= y)) || ((y == z) && (y /= x))) = Isosceles	
+--				| (x^2 + y^2 == z^2) || (z^2 + y^2 == x^2) || (x^2 + z^2 == y^2) = Rectangular		
+--				| otherwise = Other
+-------------------

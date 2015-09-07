ba78d7d8c43ad10c01e4224a655a17b477af5cdb
rdmk <robinkulhan@wcw-stud-145-109-12-31.wireless.uva.nl>
2015-09-07 14:44:08 +0200

    Added permutation function

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -7,8 +7,19 @@ data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
+
 triangle  a b c | not((a + b > c) && (b + c > a) && (c + a > b)) = NoTriangle
                 | (a == b) && (b == c) = Equilateral
                 | (a == b) || (b == c) || (a == c) = Isosceles
-                | (a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)= Rectangular
+                | (a^2 + b^2 == c^2) || (a^2 + c^2 == b^2) || (b^2 + c^2 == a^2) = Rectangular
                 | otherwise = Other
+                
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation a b = elem a (permutations b)
+
+-- ANDRE T. VERSION
+triangleA x y z  | ((x + y <= z) || (x + z <= y) || (z + y <= x)) = NoTriangle
+				 | (x == y) && (x == z) = Equilateral 
+				 | (((x == y) && (x /= z)) || ((x == z) && (x /= y)) || ((y == z) && (y /= x))) = Isosceles	
+				 | (x^2 + y^2 == z^2) || (z^2 + y^2 == x^2) || (x^2 + z^2 == y^2) = Rectangular		
+				 | otherwise = Other

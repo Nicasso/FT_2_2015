3e801aca6ba32a9cd70240b00a53bede8fd6130c
rdmk <robinkulhan@wcw-stud-145-109-12-31.wireless.uva.nl>
2015-09-07 14:01:16 +0200

    Triangle function

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -5,3 +5,10 @@ import System.Random
 
 data Shape = NoTriangle | Equilateral 
             | Isosceles | Rectangular | Other deriving (Eq,Show)
+
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle  a b c | not((a + b > c) && (b + c > a) && (c + a > b)) = NoTriangle
+                | (a == b) && (b == c) = Equilateral
+                | (a == b) || (b == c) || (a == c) = Isosceles
+                | (a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)= Rectangular
+                | otherwise = Other

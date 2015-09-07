fad88286b00bf91f94e9a577af5d56a3bba4cb25
Nico de Groot <nico@nicasso.nl>
2015-09-07 14:40:04 +0200

    Modified triangle

--- Week2/lab2.hs
+++ Week2/lab2.hs
@@ -7,8 +7,8 @@ data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle  a b c | not((a + b > c) && (b + c > a) && (c + a > b)) = NoTriangle
-                | (a == b) && (b == c) = Equilateral
-                | (a == b) || (b == c) || (a == c) = Isosceles
-                | (a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a)= Rectangular
-                | otherwise = Other
+triangle a b c | (a > (b + c)) || (b > (c + a)) || (c > (a + b)) = NoTriangle
+               | (a == b) && (b == c) = Equilateral
+               | (a == b) || (b == c) || (a == c) = Isosceles
+               | (a^2 + b^2 == c^2) || (a^2 + c^2 == b^2) || (b^2 + c^2 == a^2) = Rectangular
+               | otherwise = Other

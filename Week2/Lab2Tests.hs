2b3eb758c7b5da529c8c0435011900c1fb0a2035
astavares <astavares4@gmail.com>
2015-09-07 15:01:33 +0200

    tests ex1

--- Week2/Lab2Tests.hs
+++ Week2/Lab2Tests.hs
@@ -4,8 +4,8 @@ import Lab2
 import Testing
 
 testTriangle :: (Integer, Integer, Integer, Shape) -> Bool
-testTriangle (x, y, z, s) = (triangle x y z == s)
+testTriangle (x, y, z, s) = (triangleA x y z == s)
 
 ex1Tests :: [Test]
 ex1Tests = [ Test "triangle test" testTriangle
-             [(1, 2, 3, NoTriangle), (1, 1, 1, Equilateral)]]
+             [(1, 2, 3, NoTriangle), (1, 1, 1, Equilateral), (1, 2, 2, Isosceles), (8, 6, 10, Rectangular), (3, 5, 7, Other)]]

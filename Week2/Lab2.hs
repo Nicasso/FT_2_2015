c1ac662b8b158492dbf4f6bf18f5ad17d61b9a5b
astavares <astavares4@gmail.com>
2015-09-08 09:33:21 +0200

    some tests

--- Week2/Lab2.hs
+++ Week2/Lab2.hs
@@ -20,3 +20,32 @@ triangleA x y z  | ((x + y <= z) || (x + z <= y) || (z + y <= x)) = NoTriangle
 				 | (((x == y) && (x /= z)) || ((x == z) && (x /= y)) || ((y == z) && (y /= x))) = Isosceles	
 				 | (x^2 + y^2 == z^2) || (z^2 + y^2 == x^2) || (x^2 + z^2 == y^2) = Rectangular		
 				 | otherwise = Other
+
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation a b = elem a (permutations b)
+
+isDerangement :: [Integer] -> [Integer] -> Bool
+isDerangement a b = isPermutation a b && notEqualElemsByIndex a b
+
+--deran :: [Integer] -> [[Integer]]
+--deran [] = [[]]
+--deran (x:xs) = 	concat (map (insrt x) (perms xs)) where
+--				insrt x [] = [[x]]
+--				insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys) 
+
+notEqualElemsByIndex :: [Integer] -> [Integer] -> Bool
+notEqualElemsByIndex [] [] = True
+notEqualElemsByIndex (x:xs) (y:ys) = x /= y && notEqualElemsByIndex xs ys
+
+perms :: [a] ->[[a]]
+perms [] = [[]]
+perms (x:xs) = 	concat (map (insrt x) (perms xs)) where
+				insrt x [] = [[x]]
+				insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)	
+
+--iban :: String -> Bool			
+
+merge :: [a] -> [a] -> [a]
+merge xs     []     = xs
+merge []     ys     = ys
+merge (x:xs) (y:ys) = x : y : merge xs ys 

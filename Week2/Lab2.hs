965e54fa3d19754ca02d96d2da1380f17772369c
Nico de Groot <nico@nicasso.nl>
2015-09-11 23:02:44 +0200

    Changed the data types of the deran function

--- Week2/Lab2.hs
+++ Week2/Lab2.hs
@@ -46,7 +46,7 @@ perms (x:xs) = concat (map (insrt x) (perms xs)) where
   insrt x [] = [[x]]
   insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
 
-deran :: Integer -> [[Integer]]
+deran :: Int -> [[Int]]
 deran n = filter (\ x -> isDerangement x [0..n-1]) (perms [0..n-1])
 
 -- Implementing and testing IBAN validation

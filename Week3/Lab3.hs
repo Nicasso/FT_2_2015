c35478df24e90ce4cbf19fa21e7c56bc9b7679fb
Nico de Groot <nico@nicasso.nl>
2015-09-14 13:03:48 +0200

    Go

--- Week3/Lab3.hs
+++ Week3/Lab3.hs
@@ -3,3 +3,17 @@ module Lab3 where
 
 import Data.List
 import System.Random
+
+contradiction :: Form -> Bool
+contradiction = undefined
+
+tautology :: Form -> Bool
+tautology = undefined
+
+-- | logical entailment 
+entails :: Form -> Form -> Bool
+entails = undefined
+
+-- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv = undefined

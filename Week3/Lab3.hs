63c610898eed0e5a8f3368f2f9baae1625c59022
Nico de Groot <nico@nicasso.nl>
2015-09-15 13:13:00 +0200

    Adding the CNF converter

--- Week3/Lab3.hs
+++ Week3/Lab3.hs
@@ -33,6 +33,8 @@ equiv x y = tautology (Equiv x y)
 
 -- 2. Testing the parse function
 
+-- Time spent 1 hour
+
 testParse :: (String, [Form]) -> Bool
 testParse (x, y) = (parse x == y)
 
@@ -42,4 +44,30 @@ parseTests = [ Test "parse test" testParse
 
 -- 3. Converting formulas into CNF
 
-cnfExample = Impl p q
+-- Time spent 6 hours
+
+cnfExample1 = Equiv p q
+cnfExample2 = Impl (Impl p q) r
+cnfExample3 = Equiv p (Cnj [q, r])
+cnfExample4 = Dsj [p, (Cnj [q, r])]
+cnfExample5 = Cnj [p, (Dsj [q, r])]
+cnfExample6 = Dsj [p, (Dsj [q, r])]
+cnfExample7 = Cnj [p, (Cnj [q, r])]
+
+convertToCNF :: Form -> Form
+convertToCNF x = cnf(nnf(arrowfree x))
+
+cnf :: Form -> Form
+cnf (Prop x) = Prop x
+cnf (Neg x) = Neg x
+cnf (Cnj x) = Cnj (map cnf x)
+cnf (Dsj x) = distribute (map cnf x)
+
+distributionLaw :: Form -> Form -> Form
+distributionLaw (Cnj x) y = Cnj (map (\z -> distributionLaw z y) x)    
+distributionLaw x (Cnj y) = Cnj (map (\z -> distributionLaw x z) y)
+distributionLaw x y = Dsj [x, y]
+
+distribute :: [Form] -> Form
+distribute [x] = x
+distribute (x:xs) = distributionLaw x (distribute xs)

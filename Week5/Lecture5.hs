92eafc3795a33041def1b1e4743b8011215a8ea1
Nico de Groot <nico@nicasso.nl>
2015-09-28 14:18:57 +0200

    Modified the showGrid function

--- Week5/Lecture5.hs
+++ Week5/Lecture5.hs
@@ -24,28 +24,54 @@
   showRow :: [Value] -> IO()
   showRow [a1,a2,a3,a4,a5,a6,a7,a8,a9] = 
    do  putChar '|'         ; putChar ' '
-       putStr (showVal a1) ; putChar ' '
-       putStr (showVal a2) ; putChar ' '
+       putStr (showVal a1) ; putChar ' ';putChar ' '
+       putStr (showVal a2) ; putChar ' ';putChar ' '
        putStr (showVal a3) ; putChar ' '
        putChar '|'         ; putChar ' '
-       putStr (showVal a4) ; putChar ' '
-       putStr (showVal a5) ; putChar ' '
+       putStr (showVal a4) ; putChar ' ';putChar ' '
+       putStr (showVal a5) ; putChar ' ';putChar ' '
        putStr (showVal a6) ; putChar ' '
        putChar '|'         ; putChar ' '
-       putStr (showVal a7) ; putChar ' '
-       putStr (showVal a8) ; putChar ' '
+       putStr (showVal a7) ; putChar ' ';putChar ' '
+       putStr (showVal a8) ; putChar ' ';putChar ' '
+       putStr (showVal a9) ; putChar ' '
+       putChar '|'         ; putChar '\n'
+
+  showRow2 :: [Value] -> IO()
+  showRow2 [a1,a2,a3,a4,a5,a6,a7,a8,a9] = 
+   do  putChar '|'         ; putChar ' '
+       putStr (showVal a1) ; putChar ' ';putChar '|'
+       putStr (showVal a2) ; putChar ' ';putChar ' '
+       putStr (showVal a3) ; putChar ' '
+       putChar '|'         ; putChar ' '
+       putStr (showVal a4) ; putChar '|';putChar ' '
+       putStr (showVal a5) ; putChar ' ';putChar '|'
+       putStr (showVal a6) ; putChar ' '
+       putChar '|'         ; putChar ' '
+       putStr (showVal a7) ; putChar ' ';putChar ' '
+       putStr (showVal a8) ; putChar '|';putChar ' '
        putStr (showVal a9) ; putChar ' '
        putChar '|'         ; putChar '\n'
 
   showGrid :: Grid -> IO()
   showGrid [as,bs,cs,ds,es,fs,gs,hs,is] =
-   do putStrLn ("+-------+-------+-------+")
-      showRow as; showRow bs; showRow cs
-      putStrLn ("+-------+-------+-------+")
-      showRow ds; showRow es; showRow fs
-      putStrLn ("+-------+-------+-------+")
-      showRow gs; showRow hs; showRow is
-      putStrLn ("+-------+-------+-------+")
+   do putStrLn ("+---------+---------+---------+")
+      showRow as; 
+      putStrLn ("|   +-----|--+   +--|-----+   |")
+      showRow2 bs; 
+      showRow2 cs
+      putStrLn ("+---------+---------+---------+")
+      showRow2 ds;
+      putStrLn ("|   +-----|--+   +--|-----+   |")
+      showRow es;
+      putStrLn ("|   +-----|--+   +--|-----+   |")
+      showRow2 fs
+      putStrLn ("+---------+---------+---------+")
+      showRow2 gs; 
+      showRow2 hs; 
+      putStrLn ("|   +-----|--+   +--|-----+   |")
+      showRow is
+      putStrLn ("+---------+---------+---------+")
 
   type Sudoku = (Row,Column) -> Value
 

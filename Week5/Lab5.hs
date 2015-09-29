5e0c27bcc6662705e002959dcdf66b83fd6412eb
Nico de Groot <nico@nicasso.nl>
2015-09-29 15:09:24 +0200

    Working on assignment 4

--- Week5/Lab5.hs
+++ Week5/Lab5.hs
@@ -27,9 +27,74 @@ testMinimal :: IO ()
 testMinimal = do [r] <- rsolveNs [emptyN]
                  s <- genProblem r
                  showNode s
-                 if (uniqueSol s) == True then print ("P has a unique solution.") else error ("P does not have a unique solution, thus P is minimal")
-                 if (tryRemovingHint s (filledPositions (fst s)) 0) == True then print ("All P' of P have multiple solutions, thus P is minimal") else error ("A P' of P also has a unique solution, thus P is not minimal")
+                 if (uniqueSol s) == True then
+                  print ("P has a unique solution.") else
+                   error ("P does not have a unique solution, thus P is minimal")
+                 if (tryRemovingHint s (filledPositions (fst s)) 0) == True then
+                  print ("All P' of P have multiple solutions, thus P is minimal") else
+                   error ("A P' of P also has a unique solution, thus P is not minimal")
 
 tryRemovingHint :: Node -> [(Row,Column)] -> Int -> Bool
 tryRemovingHint s rc x | uniqueSol ( eraseN s (rc!!x) ) == True = False
                        | uniqueSol ( eraseN s (rc!!x) ) == False = if (length rc > x+1) then tryRemovingHint s rc (x+1) else True
+
+-- 4.
+
+blockies :: Int -> [(Row,Column)]
+blockies 1 = [ (x, y) | x <- [1..3], y <- [1..3]]
+blockies 2 = [ (x, y) | x <- [1..3], y <- [4..6]]
+blockies 3 = [ (x, y) | x <- [1..3], y <- [7..9]]
+blockies 4 = [ (x, y) | x <- [4..6], y <- [1..3]]
+blockies 5 = [ (x, y) | x <- [4..6], y <- [4..6]]
+blockies 6 = [ (x, y) | x <- [4..6], y <- [7..9]]
+blockies 7 = [ (x, y) | x <- [7..9], y <- [1..3]]
+blockies 8 = [ (x, y) | x <- [7..9], y <- [4..6]]
+blockies 9 = [ (x, y) | x <- [7..9], y <- [7..9]]
+
+eraseSBlock :: Sudoku -> (Row,Column) -> Sudoku
+eraseSBlock s (r,c) (x,y) | (r,c) == (x,y) = 0
+                      | otherwise      = s (x,y)
+
+eraseNBLock :: Node -> [(Row,Column)] -> Node
+eraseNBLock n ((r,c):rcs) = (s, constraints s) 
+  where s = eraseSBlock (fst n) (r,c)
+
+minimalizeBlocks :: Node -> [(Row,Column)] -> Node
+minimalizeBlocks n [] = n
+minimalizeBlocks n ((r,c):rcs) | uniqueSol n' = minimalizeBlocks n' rcs
+                               | otherwise    = minimalizeBlocks n  rcs
+   where n' = do until (>9) eraseN2 n (blockies c)
+
+
+
+
+
+eraseS2 :: Sudoku -> (Row,Column) -> Sudoku
+eraseS2 s (r,c) (x,y) | (r,c) == (x,y) = 0
+                      | otherwise      = s (x,y)
+
+eraseN2 :: Node -> (Row,Column) -> Node
+eraseN2 n (r,c) = (s, constraints s) 
+  where s = eraseS2 (fst n) (r,c) 
+
+minimalize2 :: Node -> [(Row,Column)] -> Node
+minimalize2 n [] = n
+minimalize2 n ((r,c):rcs) | uniqueSol n' = minimalize2 n' rcs
+                          | otherwise    = minimalize2 n  rcs
+   where n' = eraseN2 n (r,c)
+
+filledPositions2 :: Sudoku -> [(Row,Column)]
+filledPositions2 s = [ (r,c) | r <- positions,  
+                              c <- positions, s (r,c) /= 0 ]
+
+genProblem2 :: Node -> IO Node
+genProblem2 n = do ys <- randomize xs
+                   --return (minimalize2 n (filledPositions2 (fst (minimalizeBlocks n ys))))
+                   return (minimalizeBlocks n ys)
+   where xs = filledPositions2 (fst n)
+
+main2 :: IO ()
+main2 = do [r] <- rsolveNs [emptyN]
+           showNode r
+           s  <- genProblem2 r
+           showNode s

af32a8b58a96687e6e33c04df178ea71e39eb367
Nico de Groot <nico@nicasso.nl>
2015-10-04 16:24:39 +0200

    Adding comments.

--- Week5/Assignment5.hs
+++ Week5/Assignment5.hs
@@ -4,6 +4,13 @@ where
   
 import Assignment1
 
+-- Our NRC implementation of assignment one doesn't only make it possible to solve NRC Sudoku problems, but also to generate them.
+-- So by importing Assignment1 and creating a seperate mainNRC function we can now generate NRC Sudoku problems.
+
+-- Run using: mainNrc
+
+-- Time spent: 0.5 hours
+
 mainNrc :: IO ()
 mainNrc = do 
             [r] <- rsolveNs [emptyN]

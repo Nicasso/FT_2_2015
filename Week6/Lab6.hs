f50f0548094564c36aec2e708caa041c44fb6e7b
Nico de Groot <nico@nicasso.nl>
2015-10-05 13:43:15 +0200

    Added comments

--- Week6/Lab6.hs
+++ Week6/Lab6.hs
@@ -3,3 +3,31 @@ module Lab6 where
 import Data.List
 import System.Random
 import Lecture6
+
+-- Assignment 1
+
+
+
+-- Assignment 2
+
+
+
+-- Assignment 3
+
+
+
+-- Assignment 4
+
+
+
+-- Assignment 5
+
+
+
+-- Assignment 6
+
+
+
+-- Assignment 7
+
+
